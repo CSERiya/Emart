61616a9758bbac4bfab29c61cf27a657bb67ab33
CSERiya <93813209+CSERiya@users.noreply.github.com>
2024-06-08 23:47:15 +0530

    css properties

--- frontend/src/component/Cart/Cart.js
+++ frontend/src/component/Cart/Cart.js
@@ -7,7 +7,9 @@ const Cart = () => {
 const item= {
     product:"Bedsheet",
     price:200,
-    name:"Riya"
+    name:"Riya",
+    quantity:1,
+    image:"https://m.media-amazon.com/images/I/816rTCsPhWL._AC_UF894,1000_QL80_.jpg",
 }
 
   return (
@@ -21,6 +23,12 @@ const item= {
 
     <div className='cartContainer'>
         <CartItemCard item={item}/>
+       <div className='cartInput'>
+        <button>-</button>
+        <input type='number' value={item.quantity} readOnly />
+        <button>+</button>
+       </div>
+       <p className='cartSubtotal'>{`₹${item.price*item.quantity}`}</p>
     </div>
   </div>
   </>
